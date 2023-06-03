d415f2d82162ef08e9cc94434a17ca14051793a0
Karan Sanghvi <111078325+karansanghvi@users.noreply.github.com>
2023-06-03 20:26:05 +0530

    changes in app js file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
+let name = "Karan"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React With Karan
-        </a>
-      </header>
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+
+    <div className="container">
+      <h1>
+        Hello {name}
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit dolor mollitia, ullam minus ducimus, autem quo libero, corrupti amet rerum quisquam officia. Quam harum ratione sint voluptate nulla fugit sed aperiam culpa quae vel!
+      </p>
     </div>
+    </>
   );
 }
 
